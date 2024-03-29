637288725fa821c47d55722d566b05647b0312f8
JohannTA <71386375@usat.pe.com>
2024-03-29 14:39:55 -0500

    coreccion de contrasena

--- db/bd.py
+++ db/bd.py
@@ -5,6 +5,6 @@ def obtener_conexion():
         host='mysql-usat2024-usat-fc50.a.aivencloud.com',
         port=17580,
         user='avnadmin',
-        password='||AVNS_qPrQmXKedZ0dcoB12Ee||',
+        password='AVNS_qPrQmXKedZ0dcoB12Ee',
         db='bd_academicoCloud' )
         
