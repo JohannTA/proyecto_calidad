9d188947410e142ebbfbff864d7074cae38a08ff
Andree Requejo <apostolesdeyom@gmail.com>
2024-03-29 13:29:30 -0500

    inicio
    inicio

---
+++ db/bd.py
@@ -0,0 +1,10 @@
+import pymysql
+
+def obtener_conexion():
+    return pymysql.connect (
+        host='mysql-usat2024-usat-fc50.a.aivencloud.com',
+        port=17580,
+        user='avnadmin',
+        password='||AVNS_qPrQmXKedZ0dcoB12Ee||',
+        db='bd_academicoCloud' )
+        
